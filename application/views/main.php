0c1054cd7fc9482e0a13d01d1eb1dce6d20530bf
udots <udots@yahoo.com>
2016-02-22 18:07:40 +0700

    Test sass and susy, not worked yet

--- application/views/main.php
+++ application/views/main.php
@@ -5,6 +5,35 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css">
 </head>
 <body>
-	<h1>KRISCHAN</h1>
+	<contents>
+		<h1>KRISCHAN</h1>
+		<p>Sudah lebih dari 10 tahun kami menjalani usaha di bidang jasa servis software maupun hardware smartphone seperti Blackberry, Apple dan Android. Sudah tentu banyak sekali pengalaman yang kami dapat dan kami jadikan pelajaran untuk kami supaya kami bisa bekerja lebih baik lagi menangani kebutuhan konsumen kami.</p>
+		<p>Smartphone adalah device dengan sistem operasi yang tidak lepas dari masalah, baik masalah dari segi software maupun hardware. Oleh karena itu, disini kami hadir dan selalu siap untuk membantu memecahkan masalah smarthone anda. Support devce kami sekarang ini sudah semakin luas, untuk masalah software kami bisa menangani beberapa vendor smartphone ternama seperti Google (nexus, android one), Samsung, Sony, Smartfren, Lenovo, Oppo, Xiaomi, Asus, ZTE, Acer, Apple, Blackberry, dan lain sebagainya.</p>
+		<p>Kami menyediakan jasa untuk melayani kebutuhan anda, seperti:</p>
+	    <ul>
+	    	<li>Transfer data antar smartphone (phonebook/daftar kontak, catatan, foto, lagu, video, dokumen, dll)</li>
+				<p>Saat kita berencana untuk membeli sebuah smartphone baru, biasanya kita selalu dipusingkan dengan data yang harus kita transfer ke smartphone baru kita, seperti daftar kontak yang sangat banyak yang harus kita tambahkan satu persatu di smartphone baru kita. Belum lagi ditambah data media seperti foto, lagu dan video dimana untuk mentransfer semua data tersebut membutuhkan waktu yang sangat lama apabila dikerjakan secara manual. Untuk itu kita bisa membantu mentransfer semua data tersebut dengan waktu yang sangat cepat, sehingga bisa menghemat waktu dan tenaga anda dengan biaya yang sangat terjangkau.</p>
+	    	<li>Servis Hardware dan Software</li>
+	    		<p>Smartphone anda bermasalah? Mati total? Tidak bisa charging? Lupa kata sandi? Tidak bisa masuk menu atau biasa disebut bootloop? Kami bisa bantu memperbaiki smartphone anda baik dalam segi hardware maupun software. Untuk software kami bisa mengerjakan banyak brand terkenal, seperti: Samsung, Sony, Nexus Devices, Smartfren Devices, Asus, smartphone android China, iPhone, iPad, iPad touch, Blackberry, Lenovo, Acer, Motorola, dll.</p>
+	    	<li>Upgrade OS Smartphone</li>
+				<p>Upgrade OS adalah salah satu hal yang cukup penting untuk membuat smartphone anda tetap stabil. Beberapa kelebihan upgrade OS diantaranya: bug fix, kompatibilitas aplikasi dan games, perbaikan pengaturan hardware (RAM, baterai, dan kecepatan), dll. Untuk upgrade OS yang bagus adalah upgrade OS yang disertai clear data, sehingga OS akan lebih stabil dan fresh, dimana cara upgrade seperti ini akan menghilangkan data total, kami bisa bantu upgrade OS yang disertai clear data tanpa menghilangkan data anda, karena data kami backup terlebih dahulu, dan dengan pengerjaan yang cukup cepat.</p>
+		    <li>Jailbreak iOS devices dan Rooting Android</li>
+				<p>Kita tentu mengenal apa itu jailbreak maupun rooting. Keduanya memiliki kesamaan yaitu untuk membuka kunci sistem OS dari smartphone yang kita miliki. Banyak sekali manfaat dari jailbreak maupun rooting, tetapi pengerjaan jailbreak maupun rooting tentu ada resikonya, salah satunya adalah errornya OS yang bisa membuat smartphone kita bootloop bahkan mati total. Maka dari itu kami bisa membantu kebutuhan anda untuk jailbreak maupun rooting sehingga resiko bisa di minimalisir.</p>
+			<li>Aplikasi dan Games full version</li>
+				<p>Kami juga menyediakan beberapa aplikasi penting untuk smartphone Apple dan Android, seperti: GPS offline (satellite based GPS), AL-Qurâ€™an yang disertai terjemahan dan audio, Office (Word, Excel, PowerPoint, PDF reader), Games HD dan SD, dll.</p>
+	    	<li>Recovery data sdcard</li>
+				<p>Data foto dan video di sdcard anda hilang karena tidak sengaja kehapus atau keformat? Padahal data tersebut sangat penting bagi anda. Kami bisa bantu mencoba untuk mengembalikan data tersebut dengan waktu yang cukup cepat, asalkan sdcard anda belum terisi data baru.</p>
+		</ul>
+		<p>Selain dari jasa-jasa di atas, kami juga menyediakan layanan jasa untuk pembuatan akun email, Apple store, bbm, dll, serta jasa-jasa lainnya seputar smartphone yang anda butuhkan.</p>
+	</contents>
+	<sideright>
+		<ul>
+			<li>Option 1</li>
+			<li>Option 2</li>
+			<li>Option 3</li>
+			<li>Option 4</li>
+			<li>Option 5</li>
+		</ul>
+	</sideright>
 </body>
 </html>
